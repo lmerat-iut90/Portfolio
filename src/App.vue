0ca63617aa06850ac8d41bacfd0019f5815db10e
lmerat-iut90 <leslie.merat@edu.univ-fcomte.fr>
2025-04-28 15:06:19 +0200

    first

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/Photo.jpg">
+    <HelloWorld msg="Welcome to my Portfolio"/>
   </div>
 </template>
 
